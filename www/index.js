ec8aea097a15878566c9140da58782dc2bf0c55d
Ye Liu <ye@basecase.co>
2016-03-20 00:46:12 -0400

    Removed delay when receiving playing event

--- www/index.js
+++ www/index.js
@@ -17,39 +17,39 @@ function onDeviceReady () {
 function playVideo (video, canvas, videoSrc) {
   var maxWidth = 200
   var pixelRatio = window.devicePixelRatio || 2
+  console.log('devicePixelRatio = ' + window.devicePixelRatio)
 
   video.src = videoSrc
   video.width = maxWidth
-  video.play()
 
   video.addEventListener('playing', function () {
-    setTimeout(function () {
-      var vw = video.videoWidth
-      var vh = video.videoHeight
-      var aspectRatio = vw / vh
-      var cw = maxWidth * pixelRatio
-      var ch = cw / aspectRatio
-      canvas.width = cw
-      canvas.height = ch
-      canvas.style.width = cw / pixelRatio + 'px'
-      canvas.style.height = ch / pixelRatio + 'px'
-      var ctx = canvas.getContext('2d')
+    var vw = video.videoWidth
+    var vh = video.videoHeight
+    var aspectRatio = vw / vh
+    var cw = maxWidth * pixelRatio
+    var ch = Math.ceil(cw / aspectRatio)
+    canvas.width = cw
+    canvas.height = ch
+    canvas.style.width = maxWidth + 'px'
+    canvas.style.height = Math.ceil(maxWidth / aspectRatio) + 'px'
+    var ctx = canvas.getContext('2d')
 
-      function drawFrame () {
-        ctx.save()
-        // translate to the center of the rectangle
-        ctx.translate(cw * 0.5, ch * 0.5)
-        // rotate 90 degree to the right
-        ctx.rotate(Math.PI * 0.5)
-        // translate back
-        ctx.translate(-ch * 0.5, -cw * 0.5)
-        // draw frame with rotated dimension
-        ctx.drawImage(video, 0, 0, ch, ch / aspectRatio)
-        ctx.restore()
-        setTimeout(drawFrame)
-      }
+    function drawFrame () {
+      ctx.save()
+      // translate to the center of the rectangle
+      ctx.translate(cw * 0.5, ch * 0.5)
+      // rotate 90 degree to the right
+      ctx.rotate(Math.PI * 0.5)
+      // translate back
+      ctx.translate(-ch * 0.5, -cw * 0.5)
+      // draw frame with rotated dimension
+      ctx.drawImage(video, 0, 0, ch, Math.ceil(ch / aspectRatio))
+      ctx.restore()
+      setTimeout(drawFrame)
+    }
 
-      drawFrame()
-    }, 10)
+    drawFrame()
   }, false)
+
+  video.play()
 }
